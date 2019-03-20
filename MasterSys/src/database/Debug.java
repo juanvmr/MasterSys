5f86a963786f9c8309568cda8d5a0aba8bbf6c12
Luaerror <pedrozc90@gmail.com>
2019-03-20 19:46:19 -0300

    new Menu.java

--- MasterSys/src/database/Debug.java
+++ MasterSys/src/database/Debug.java
@@ -3,9 +3,15 @@ package database;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+import panels.*;
+import database.models.*;
+import database.dao.*;
+
 public class Debug {
 
 	public static void main(String[] args) {
+		
+		new Menu();
 		
 		Connection conn = ConnectionFactory.getConnection("master", "admin", "admin");
 		try {
